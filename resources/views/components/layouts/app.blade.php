326bf32a621d78a8508e8930cebf1d15d324d46a
Siulacio <siulacio@hotmail.com>
2022-11-08 14:15:52 -0500

    9. Plantillas de Blade - Componentes

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,12 +5,11 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Siulacio - @yield('title')</title>
-    <meta name="description" content="@yield('meta-description', 'Default meta description')">
+    <title>Siulacio - {{ $title ?? '' }}</title>
+    <meta name="description" content="{{ $metaDescription ?? 'Default meta description' }}" />
 </head>
 <body>
-@include('partials.navigation')
-@yield('content')
+    <x-layouts.navigation/>
+    {{ $slot }}
 </body>
 </html>
-
