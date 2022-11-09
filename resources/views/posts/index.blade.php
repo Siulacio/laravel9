44357a3dbe50b8345b82ff9b8fbc092ea9a30dc5
Siulacio <siulacio@hotmail.com>
2022-11-09 11:22:03 -0500

    14. consultar registros por ID

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,6 +1,10 @@
 <x-layouts.app title="Blog" meta-description="Blog meta description">
     <h1>Blog</h1>
     @foreach($posts as $post)
-        <h2>{{ $post->title }}</h2>
+        <h2>
+            <a href="{{ route('posts.show', $post) }}">
+                {{ $post->title }}
+            </a>
+        </h2>
     @endforeach
 </x-layouts.app>
