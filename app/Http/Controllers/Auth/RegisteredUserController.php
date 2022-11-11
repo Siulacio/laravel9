c2f2686394cb365d4c930ce04360c770da54953a
Siulacio <siulacio@hotmail.com>
2022-11-11 14:39:16 -0500

    31. formulario de registro de usuarios

---
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Validation\Rules;
+use Illuminate\Http\Request;
+
+class RegisteredUserController extends Controller
+{
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => bcrypt($request->input('password')),
+        ]);
+
+        return to_route('login')->with('status', 'Account created');
+    }
+}
